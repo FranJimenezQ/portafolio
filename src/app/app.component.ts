27b089da0e1dd275a89e2824a605d9305e372453
francisco jimenez <flakundo@gmail.com>
2024-03-21 20:35:35 -0600

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AboutComponent, SkillsComponent, ContactComponent, ProjectsComponent, NgIf]
 })
 export class AppComponent {
   title = 'francisco-jimenez-portafolio';
+
+  visibleSections = {
+    about: false,
+    skills: false,
+    contact: false,
+    projects: false
+  }
+
+  toggleDiv(section: keyof typeof this.visibleSections){
+    Object.keys(this.visibleSections).forEach((key) => {
+      this.visibleSections[key as keyof typeof this.visibleSections] = false;
+    });
+    this.visibleSections[section] = true;
+  }
 }
+
+// this.visibleSections[section] = !this.visibleSections[section] 
+// if (this.visibleSections.skills == true) {
+//     this.visibleSections.contact = false;
+//     this.visibleSections.projects = false;
+
+// }
+// if (this.visibleSections.contact) {
+//   this.visibleSections.skills = false;
+//   this.visibleSections.projects = false
+// }
+// if (this.visibleSections.projects) {
+//   this.visibleSections.contact = false;
+//   this.visibleSections.skills = false;
+// }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+];
