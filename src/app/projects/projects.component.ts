bbfb697e709b9455f91857f322eb1425572d21a4
francisco jimenez <flakundo@gmail.com>
2024-06-02 09:10:17 -0600

    images path fix

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,37 +12,38 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  baseHref: string = 'portafolio'
 
   projectsSlides = [
     {
-      url: './../../assets/images/memoryGame.png',
+      url: '/portafolio/assets/images/memoryGame.png',
       title: "Memory Game",
       description: "Memory game developed with JavaScript",
       projectUrl: "https://franjimenezq.github.io/memory-game/"
     },
     {
-      url: "./../../assets/images/drinksApp.png",
+      url: "/portafolio/assets/images/drinksApp.png",
       title: "Drinks App",
       description: "Drinks app created with Angular",
       projectUrl: "https://franjimenezq.github.io/drinks-app/"
 
     },
     {
-      url: "./../../assets/images/notiWeb.png",
+      url: "/portafolio/assets/images/notiWeb.png",
       title: "NotiWeb",
       description: "News website built with HTML, CSS and Javascript",
       projectUrl: "https://franjimenezq.github.io/Noti-Web/"
 
     },
     {
-      url: "/assets/images/rottenMovies38.png",
+      url: "/portafolio/assets/images/rottenMovies38.png",
       title: "Rotten Movies 38",
       description: "Movie rating app using Angular",
       projectUrl: "https://franjimenezq.github.io/movie-Angular-App/"
 
     },
     {
-      url: "/assets/images/webTemplate.png",
+      url: "/portafolio/assets/images/webTemplate.png",
       title: "Web Template",
       description: "Web template created with HTML & CSS",
       projectUrl: "https://franjimenezq.github.io/Flakundo-Web-Template1/"
